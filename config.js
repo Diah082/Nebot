88d00d207d104cde6d01fa2f9d8e40987e1b263e
Diah082 <152632332+Diah082@users.noreply.github.com>
2024-07-28 02:02:52 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,98 @@
+
+const fs = require("fs");
+const chalk = require("chalk");
+require("dotenv").config();
+
+// Default values for global variables
+global.available = process.env.AVAILABLE || true;
+global.autoReadAll = process.env.AUTO_READ_ALL || false;
+global.antitags = process.env.ANTITAGS || true;
+
+// Auto functioner
+global.autoTyping = process.env.AUTO_TYPING || false;
+global.autoRecord = process.env.AUTO_RECORD || false;
+global.groupevent = process.env.GROUPEVENT || false;
+global.statusseen = process.env.STATUSSEEN || true;
+global.autoreadgc = process.env.AUTOREADGC || true;
+
+
+// Auth information
+global.pairNumber = "6282326322300";                         // Add your paining number with country code example "6282326322300"; 
+global.port = process.env.PORT || "10000";
+global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
+global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
+global.mongodb = process.env.MONGODB || "";                 // Mongodb url.
+global.website = "https://www.exenoz.tech"; 
+global.github = "https://github.com/Diah082";
+
+
+// Default prefix
+global.prefa = process.env.PREFIX ? process.env.PREFIX.split(".") : [","];
+
+
+// Owner information
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["6282326322300"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["6282326322300"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["6282326322300"];
+global.OwnerName = process.env.OWNER_NAME || "Newbie";
+global.BotName = process.env.BOT_NAME || "Newbie";
+global.packname = process.env.PACK_NAME || "NEWBIE Bot";
+global.author = "By: Newbie";
+global.BotSourceCode = "https://github.com/Diah082/NewbieBot";
+global.SupportGroupLink = "https://chat.whatsapp.com/Jo9QobvI8Ge2LdFVNL6Mlp";
+
+
+//
+global.openAiAPI = process.env.OPENAI_API || "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";
+global.location = process.env.LOCATION || "Center Java, Indonesia";
+global.reactmoji = process.env.REACT_MOJI || "❤️";
+global.themeemoji = process.env.THEME_EMOJI || "💖";
+global.vidmenu = { url: process.env.VID_MENU_URL || 'https://telegra.ph/file/ae16bc14d33d7d520cd7d.mp4' };
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+global.them = "https://r4.wallpaperflare.com/wallpaper/1003/376/845/makoto-shinkai-kimi-no-na-wa-wallpaper-0816ade8b0301c58302c014e48d2441a.jpg";
+
+
+//
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+// Messages
+global.mess = {
+  jobdone: 'Here you go...',
+  useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+  botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+  botowner: 'Only my *Owner* can use this command, Baka!',
+  grouponly: 'This command is only made for *Groups*, Baka!',
+  privateonly: 'This command is only made for *Private Chat*, Baka!',
+  botonly: 'Only the *Bot itself* can use this command!',
+  waiting: 'Just Wait...',
+  nolink: 'Please provide me *link*, Baka!',
+  error: 'An error occurd!',
+  banned: 'You are *Banned* fron using commands!',
+  bangc: 'This Group is *Banned* from using Commands!',
+  nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+
+}
