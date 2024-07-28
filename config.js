e00e17f3ffd07fe9190b7f7a0ee5d7d7d85b44d0
Diah082 <152632332+Diah082@users.noreply.github.com>
2024-07-28 15:53:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = "6282326322300";                         // Add your paining number with country code example "6282326322300"; 
+global.pairNumber = "XXXX";                         // Add your paining number with country code example "6282326322300"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
@@ -27,7 +27,7 @@ global.github = "https://github.com/Diah082";
 
 
 // Default prefix
-global.prefa = process.env.PREFIX ? process.env.PREFIX.split(".") : [","];
+global.prefa = process.env.PREFIX ? process.env.PREFIX.split(".") : ["/"];
 
 
 // Owner information
